1f1495afab283bb9288af9535b2520a4a08300fb
MarckJrquin <marck0208@outlook.com>
2023-05-25 22:32:25 -0500

    inicio de proyecto Parcial 2, DSVI

---
+++ app/src/main/java/com/example/parcial2_csanchez_evergara_mnurinda/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parcial2_csanchez_evergara_mnurinda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
